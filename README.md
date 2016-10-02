15392c00ee9f6736e2b2d2fda88885bf7a9cfaf8
Sven Schannak <sven@Svens-MBP.fritz.box>
2016-10-02 13:01:43 +0200

    changes config from environment to config file

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 
 ## Installation
+### Linux
 ```bash
 sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
+```
+
+### macOS
+```bash
+sudo npm install -g phantomjs-prebuilt
 ```
--- firebase_connector.py
+++ firebase_connector.py
@@ -1,24 +1,15 @@
 import pyrebase
 import datetime
 import os
-
-config = {
-  "apiKey": os.environ["MONITHORING_FB_API_KEY"],
-  "authDomain": os.environ["MONITHORING_FB_AUTH_DOMAIN"],
-  "databaseURL": os.environ["MONITHORING_FB_DB_URL"],
-  "storageBucket": os.environ["MONITHORING_FB_STORAGE_BUCKET"],
-  "serviceAccount": "service_file/service_account.json"
-}
-
+from conf import CONFIG
 
 DATE_FORMAT = "%Y-%m-%d-%H-%M"
-
 
 class FirebaseConnector:
 
     def __init__(self):
 
-        self.firebase = pyrebase.initialize_app(config)
+        self.firebase = pyrebase.initialize_app(CONFIG)
         self.db = self.firebase.database()
         self.storage = self.firebase.storage()
         self.current_time = datetime.datetime.now().strftime(DATE_FORMAT)
@@ -32,4 +23,3 @@ class FirebaseConnector:
     def upload_screenshot_and_get_url(self, user_id, filename):
         result = self.storage.child("user").child(user_id).child("%s" % filename).put(filename)
         return self.storage.child("user").child(user_id).child("%s" % filename).get_url()
-
